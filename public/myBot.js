01e9b6a237b8f73b4d1e02be30736fbd8f6315c3
AlexMarvelo <mironenkoalexey@gmail.com>
2017-10-08 14:33:14 +0300

    Improve Goalkeeper slow stop mode

--- public/myBot.js
+++ public/myBot.js
@@ -5,12 +5,21 @@
 
 'use strict'; // eslint-disable-line
 
+const PI = Math.PI;
+
 const GOALKEEPER = 1;
 // const PLAYER2 = 0;
 // const PLAYER3 = 2;
 
-const GOALKEEPER_POS_X = 0.056; // % of field width
-const EPS = 1;
+const GOALKEEPER_POS_X = 0.07; // % of field width
+const GoalkeeperModes = {
+  FOLLOW: 'FOLLOW', // align Y with the ball, but keep goalkeeper X distance
+  DEFENCE: 'DEFENCE', // move toward the ball
+  DEFAULT: 'FOLLOW',
+};
+
+// const POS_EPS = 1;
+// const DIR_EPS = PI / 180;
 
 function getPlayerMove(data) {
   const currentPlayer = data.yourTeam.players[data.playerIndex];
@@ -40,32 +49,56 @@ function calculateGoalkeeperMove(data) {
   const ball = data.ball;
   const ballStop = getBallStats(ball, data.settings);
   const player = data.yourTeam.players[data.playerIndex];
+
   const fieldWidth = data.settings.field.width;
-  const goalkeeperZoneEnd = GOALKEEPER_POS_X * 3 * fieldWidth;
+  // const fieldHeight = data.settings.field.height;
+  const ballRadius = data.settings.ball.radius;
+  const playerRadius = data.settings.player.radius;
+  const maxPlayerVelocity = data.settings.player.maxVelocity;
+  const goalkeeperZoneEnd = GOALKEEPER_POS_X * 4 * fieldWidth;
+
+  const mode = ballStop.x < goalkeeperZoneEnd ?
+    GoalkeeperModes.DEFENCE :
+    GoalkeeperModes.FOLLOW;
+
   let moveDirection = player.direction;
   let moveVelocity = 0;
 
-  if (doesGoalkeeperShouldMove(player, ball, ballStop)) { // Need to move
-    if (ballStop.x < goalkeeperZoneEnd) {
+  // ballStop.x = fieldWidth / 2;
+  // ballStop.y = fieldHeight * 0.1;
 
-      // TODO move toward the ball
-      moveDirection = getDirectionTo(player, {
-        x: ballStop.x - data.settings.player.radius,
-        y: ballStop.y,
-      });
-      moveVelocity = data.settings.player.maxVelocity;
+  // if (!shouldGoalkeeperMove(mode, player, ball, ballStop)) {
+  //   return {
+  //     direction: moveDirection,
+  //     velocity: moveVelocity,
+  //   };
+  // }
 
-    } else {
+  const currentPoint = player;
+  const targetPoint = mode === GoalkeeperModes.FOLLOW ? {
+    x: GOALKEEPER_POS_X * fieldWidth,
+    y: ballStop.y,
+  } : {
+    x: ballStop.x - ballRadius,
+    y: ballStop.y,
+  };
+  const targetDirection = getDirectionTo(currentPoint, targetPoint);
+  const targetDistance = getDistance(currentPoint, targetPoint);
 
-      // TODO align Y with the ball, but keep goalkeeper X distance
-      moveDirection = getDirectionTo(player, {
-        x: GOALKEEPER_POS_X * fieldWidth,
-        y: ballStop.y,
-      });
-      moveVelocity = data.settings.player.maxVelocity;
-
-    }
-  } // esle stop player
+  moveDirection = targetDirection;
+  const directionDelta = targetDirection - convertEngineDirection(player.direction);
+  moveVelocity = getGoalkeeperVelocity(
+    mode,
+    getPlayerVelocity(
+      player.velocity,
+      directionDelta,
+      maxPlayerVelocity,
+      2,
+    ),
+    targetDistance,
+    playerRadius,
+    maxPlayerVelocity,
+  );
 
   return {
     direction: moveDirection,
@@ -73,11 +106,42 @@ function calculateGoalkeeperMove(data) {
   };
 }
 
-function doesGoalkeeperShouldMove(goalkeeper, ball, ballStop) {
-  if (ballStop.stopTime < EPS && Math.abs(ball.y - goalkeeper.y) < EPS) {
-    return false;
+// function shouldGoalkeeperMove(mode, goalkeeper, ball, ballStop) {
+//   switch (mode) {
+//     case GoalkeeperModes.FOLLOW:
+//       if (ballStop.stopTime === 0 && Math.abs(ball.y - goalkeeper.y) < POS_EPS) {
+//         return false;
+//       }
+//       break;
+//     default:
+//   }
+//   return true;
+// }
+
+function getGoalkeeperVelocity(
+  mode = GoalkeeperModes.DEFAULT,
+  velocity,
+  distance,
+  playerRadius,
+  maxPlayerVelocity,
+) {
+  switch (mode) {
+    case GoalkeeperModes.FOLLOW:
+      return slowStopGoalkeeper(velocity, distance, playerRadius, maxPlayerVelocity);
+    case GoalkeeperModes.DEFENCE:
+      return velocity;
+    default:
   }
-  return true;
+  return null;
+}
+
+function slowStopGoalkeeper(velocity, distance, playerRadius, maxPlayerVelocity) {
+  let newVelocity = velocity;
+  const stopThreshold = playerRadius * (velocity / maxPlayerVelocity) * 8;
+  if (distance < stopThreshold) {
+    newVelocity = 0;
+  }
+  return newVelocity;
 }
 
 
@@ -115,9 +179,28 @@ function getDirectionTo(startPoint, endPoint) {
   return Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
 }
 
-// function getDistance(point1, point2) {
-//   return Math.hypot(point1.x-point2.x, point1.y - point2.y);
-// }
+function getDistance(point1, point2) {
+  return Math.hypot(point1.x - point2.x, point1.y - point2.y);
+}
+
+function getPlayerVelocity(
+  currentVelocity,
+  directionDelta,
+  maxVelocity,
+  exponent = 1,
+) {
+  const velocity = Math.max(
+    maxVelocity - (
+      Math.pow((Math.abs(directionDelta) / (PI / 2)), exponent) * currentVelocity
+    ),
+    0,
+  );
+  return velocity;
+}
+
+function convertEngineDirection(engineDirection) {
+  return engineDirection > PI ? engineDirection - (2 * PI) : engineDirection;
+}
 
 // -------------------------------------
 onmessage = e => postMessage(getPlayerMove(e.data));
